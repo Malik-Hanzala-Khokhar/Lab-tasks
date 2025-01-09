8e3bfae4f949bc70ddd46c796ee590413ef12d82
Malik-Hanzala-Khokhar <70174060@student.uol.edu.pk>
2025-01-09 18:24:13 +0500

    lab task

---
+++ labq5-1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int operation;
+    double num1,num2;
+    cout<<"choose an operation(1.+2.-3.*4./):";
+    cin>>operation;
+    cout<<"enter first num";
+    cin>>num1;
+    cout<<"enter second num";
+    cin>>num2;
+    switch(operation){
+        case'1.+'
+        
+
+        
+
+    }
+}
