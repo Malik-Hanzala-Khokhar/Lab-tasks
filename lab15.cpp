1d7c9733aec07a5907b4a56d1960cbca0c93dd61
Malik-Hanzala-Khokhar <70174060@student.uol.edu.pk>
2025-01-09 18:20:12 +0500

    lab 15

---
+++ lab15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // parking slots for cars
+    int seatsAvailable =10;
+    string carNme;
+    while(seatsAvailable >0){
+        cout<<"aCar"<<carNme<<"enter in the grage"<< endl;
+        cin>>carNme;
+      cout<<"enter in the grage";
+
+      seatsAvailable --;
+    }
+    cout<<"the grage is full"<<endl;
+return 0;
+
+}
